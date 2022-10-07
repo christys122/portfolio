432476aefcca5e8c7f3343a3d782dc7a525fbc2c
unknown <mcsalazar122@gmail.com>
2022-10-06 19:01:05 -0500

    add project and styling

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+
 
 export default function Home() {
   return (
     <div>
       <h1>Chisty Salazar ~ Web Developer</h1>
-      <section className="my-5">
+      <section className="my-6">
         <h2 id="about">Welcome to My Portfolio!</h2>
         
         <p>My name is Christy Salazar ~  I am a Full Stack Web Developer <span>Please click the links above to get to know me.  I am available for freelance opportunities across multiple platforms.  I look forward to hearing from you!</span>
     </p>
-    <img src={coverImage} className="my-2" style={{ width: "40%", height: "20%" }} alt="sunset" />
+    
     </section>
     </div>
     )  
